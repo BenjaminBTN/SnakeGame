73166e8bc5f5c8d6de40b9b28d45a13642703482
BenjaminBTN <alekseykufu@yandex.ru>
2023-02-15 23:58:16 +0300

    second

---
+++ src/MainWindow.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class MainWindow extends JFrame {
+    public MainWindow() {
+        setTitle("Змейка");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(320, 320);
+        setLocation(400, 400);
+        setResizable(false);
+        add(new GameField());
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        MainWindow mw = new MainWindow();
+    }
+}
